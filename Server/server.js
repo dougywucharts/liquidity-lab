20063945795f3beab3c8d8d7d8c5df58b28e34a3
Douglas Walton <dougywucharts@gmail.com>
2026-05-21 18:28:12 -0400

    Add express json parser

--- Server/server.js
+++ Server/server.js
@@ -18,6 +18,9 @@ app.use(
   })
 )
 
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ extended: true }))
+
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcryptjs'
 import Stripe from 'stripe'
